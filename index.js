0f262121c0be5775231b0f981426d3f68028af73
jee <jag918@nyu.edu>
2018-03-30 19:03:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 function shout(string){
   return string.toUpperCase()
 }
-
 function whisper(string){
   return string.toLowerCase()
 }
-
 function logShout(string){
   console.log(string.toUpperCase())
 }
 function logWhisper(){
   console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  if string.isUpper(){
+    return "I Can't hear you!"
+  }else{
+    return "YES INDEED!"
+  }
+  
+
 }
