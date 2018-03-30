c14c42dfaf357cfd1b9b5c344592447493b06bdf
jee <jag918@nyu.edu>
2018-03-30 19:16:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string){
   if (string.toUpperCase()===string){
     return "YES INDEED!"
   } else if (string.toLowerCase()===string){
-    return "I Can't hear you!"
+    return "I can't hear you!"
   } else{
     return "I love you, too."
   }
