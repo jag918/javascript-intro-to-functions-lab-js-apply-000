4047c408bf8ff1950d171d767aa39607afbc7f50
jee <jag918@nyu.edu>
2018-03-30 19:13:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function logWhisper(){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if string.toUpperCase()===string{
+  if (string.toUpperCase()===string){
     return "I Can't hear you!"
-  } else if string.toLowerCase()===string{
+  } else if (string.toLowerCase()===string){
     return "YES INDEED!"
   } else{
     return "I love you, too."
